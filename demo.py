74ad06fe6a9bfabb272522f1104b688a577c9f22
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2020-01-20 23:16:56 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -154,7 +154,8 @@ def run_demo(net, image_provider, height_size, cpu, track_ids):#, filename):
         
         str = "FPS : %0.2f" % fps
         cv2.putText(img, str, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-        cv2_imshow('Lightweight Human Pose Estimation Python Demo', img)
+#         cv2.imshow('Lightweight Human Pose Estimation Python Demo', img)
+        cv2.imshow(img)
         cv2.imwrite('PAF_'+str(idxx), img)
         idxx+=1
 
